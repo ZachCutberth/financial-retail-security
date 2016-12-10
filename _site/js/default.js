de5b4db1c5207d597cb6d29302c75f7968f971e4
Zach Cutberth <akston@gmail.com>
2016-12-10 01:13:11 -0800

    Improved navbar function

--- _site/js/default.js
+++ _site/js/default.js
@@ -6,9 +6,21 @@ $(document).ready(function(){
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $(".navbar-default").css('background-color', '#1e2133');
+          $(".navbar-default").css('background-color', '#fff');
+          $(".navbar-default .navbar-nav>li>a").css('color', '#000');
+          $(".navbar-default .navbar-header>a").css('color', '#000');
+          $("#nav-contact").css('padding-right', '400px');
+          $(".navbar-brand").css('padding-left', '400px');
+          $(".navbar-default .navbar-nav>li>a").css('padding-top', '20px');
+          $(".navbar-brand").css('padding-top', '27px');
        } else {
           $('.navbar-default').css('background-color', 'transparent');
+          $(".navbar-default .navbar-nav>li>a").css('color', '#fff');
+          $(".navbar-default .navbar-header>a").css('color', '#fff');
+          $("#nav-contact").css('padding-right', '50px');
+          $(".navbar-brand").css('padding-left', '50px');
+          $(".navbar-default .navbar-nav>li>a").css('padding-top', '50px');
+          $(".navbar-brand").css('padding-top', '50px');
        }
    });
     }
